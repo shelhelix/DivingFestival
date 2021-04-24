8fca34cbe352a07b34de0fe56d3b1f6f9e67e3b8
alexandr.shelekhin <alexandr.shelekhin@matryoshka.com>
2021-04-24 21:12:29 +0700

    added simplified diving background + ship

---
+++ Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+
+public class ParallaxController : MonoBehaviour {
+    [Serializable]
+    public class ParallaxObject {
+        public Transform Object;
+        public float     SpeedMultiplier = 1;
+    }
+
+    // Speed in units per second
+    public float Speed = 1;
+    
+    public List<ParallaxObject> Objects;
+
+    void Update() {
+        var offset = Speed * Time.deltaTime;
+        foreach ( var obj in Objects ) {
+            obj.Object.position += (offset * obj.SpeedMultiplier) * Vector3.up;
+        }
+    }
+}
