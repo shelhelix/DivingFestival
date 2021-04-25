8b2812b430e1d01d0561db7d68987800a31f189a
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-04-25 20:39:41 +0700

    added howtoplay screen

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -3,17 +3,34 @@ using UnityEngine.SceneManagement;
 
 using GameComponentAttributes;
 using GameComponentAttributes.Attributes;
+using UnityEngine;
 
 namespace LD48Project {
 	public class StartMenu : GameComponent {
 		[NotNull] public Button StartGame;
+		[NotNull] public Button OpenHowToScreenButton;
+		[Header("how to")] 
+		[NotNull] public Button ReturnToMainMenuButton;
 
+		[NotNull] public GameObject MainScreenRoot;
+		[NotNull] public GameObject HowToScreenRoot;
+		
 		void Start() {
 			StartGame.onClick.AddListener(() => SceneManager.LoadScene("Gameplay"));			
+			OpenHowToScreenButton.onClick.AddListener(() => {
+				MainScreenRoot.SetActive(false);
+				HowToScreenRoot.SetActive(true);
+			});
+			ReturnToMainMenuButton.onClick.AddListener(() => {
+				MainScreenRoot.SetActive(true);
+				HowToScreenRoot.SetActive(false);
+			});
 		}
 
-		private void OnDestroy() {
+		void OnDestroy() {
 			StartGame.onClick.RemoveAllListeners();
+			OpenHowToScreenButton.onClick.RemoveAllListeners();
+			ReturnToMainMenuButton.onClick.RemoveAllListeners();
 		}
 	}
 }
