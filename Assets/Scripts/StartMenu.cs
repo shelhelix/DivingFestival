4697e4cdb66c2e710dff763409b09a8e71381e0a
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-04-25 12:14:11 +0700

    added start menu scene

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using GameComponentAttributes;
+using GameComponentAttributes.Attributes;
+
+namespace LD48Project {
+	public class StartMenu : GameComponent {
+		[NotNull] public Button StartGame;
+
+		void Start() {
+			StartGame.onClick.AddListener(() => SceneManager.LoadScene("Gameplay"));			
+		}
+
+		private void OnDestroy() {
+			StartGame.onClick.RemoveAllListeners();
+		}
+	}
+}
