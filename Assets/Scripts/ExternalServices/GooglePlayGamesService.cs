a2804a77f73a0fd9b121c97cd52e1ceddb9ee522
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-09-08 16:08:19 +0700

    added basic service for the achievements and highscores

---
+++ Assets/Scripts/ExternalServices/GooglePlayGamesService.cs
@@ -0,0 +1,43 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using LD48Project.Utils;
+using UnityEngine;
+
+namespace LD48Project.ExternalServices {
+	public class GooglePlayGamesService : Singleton<GooglePlayGamesService> {
+
+		bool _isLoggedIn;
+		bool _isInited;
+
+		public void Init() {
+			if ( _isInited ) {
+				return;
+			}
+			_isInited                         = true;
+			PlayGamesPlatform.DebugLogEnabled = true;
+			PlayGamesPlatform.Activate();
+			PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, status => {
+				_isLoggedIn = status == SignInStatus.Success;
+				if ( !_isLoggedIn ) {
+					Debug.LogError($"Can't log in. Reason: {status}");
+				}
+			} );
+		}
+
+		public void PublishScore(string highscoreTableId, long score) {
+			if ( !_isLoggedIn ) {
+				Debug.LogError("Can't report score - user not logged in");
+				return;
+			}
+			Social.ReportScore(score, highscoreTableId, null);
+		}
+
+		public void SetProgressToAchievement(string achievementId, float progressPercent) {
+			if ( !_isLoggedIn ) {
+				Debug.LogError("Can't set progress to the achievement - user not logged in");
+				return;
+			}
+			Social.ReportProgress(achievementId, progressPercent, null);
+		}
+	}
+}
