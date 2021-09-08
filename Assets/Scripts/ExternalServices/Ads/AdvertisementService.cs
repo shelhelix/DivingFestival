8b187ecc42b2b657bf64dc5840a83e432e74f675
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-09-08 16:07:59 +0700

    removed LoadAd method from service cause Ad loads automatically

--- Assets/Scripts/ExternalServices/Ads/AdvertisementService.cs
+++ Assets/Scripts/ExternalServices/Ads/AdvertisementService.cs
@@ -5,14 +5,17 @@ using UnityEngine.Advertisements;
 namespace LD48Project.ExternalServices.Ads {
 	public class AdvertisementService : Singleton<AdvertisementService> {
 		AdvertisementWrapper _rewardAdWrapper;
+
+		bool _isInited;
+		
 		public void Init() {
+			if ( _isInited ) {
+				return;
+			}
+			_isInited        = true;
 			_rewardAdWrapper = new AdvertisementWrapper(AdvertisementConsts.RewardAdAndroid);
 			Advertisement.Initialize(AdvertisementConsts.GameId);	
 			Advertisement.AddListener(_rewardAdWrapper);
-		}
-
-		public void LoadAd() {
-			_rewardAdWrapper.LoadAd();
 		}
 
 		public void ShowAd(Action<bool> onAdShowed) {
