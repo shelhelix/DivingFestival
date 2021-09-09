7ac864b6e5ef9b33c6a08b0dfcc03e7e3cbfe620
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-09-09 23:43:34 +0700

    added debug texts to the main menu

---
+++ Assets/Scripts/UI/Debug/GpgsStatusUpdater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GameComponentAttributes;
+using GameComponentAttributes.Attributes;
+using LD48Project.ExternalServices;
+using TMPro;
+
+namespace LD48Project.UI.Debug {
+	public class GpgsStatusUpdater : GameComponent {
+		[NotNull] public TMP_Text Text;
+
+		public void Update() {
+			Text.text =
+				$"Is GPGS Logged in: {GooglePlayGamesService.Instance.IsLoggedIn}. Reason: {GooglePlayGamesService.Instance.LoginFailReason}";
+		}
+	}
+}
---
+++ Assets/Scripts/UI/Debug/VersionUpdater.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameComponentAttributes;
+using GameComponentAttributes.Attributes;
+using TMPro;
+using UnityEngine;
+
+public class VersionUpdater : GameComponent {
+	[NotNull] public TMP_Text Text;
+    // Update is called once per frame
+    void Update() {
+		Text.text = $"{Application.version}";
+	}
+}
