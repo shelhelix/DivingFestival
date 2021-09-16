75e7e17f36bee81ba65abcd58806cf3fb32ec7b4
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-09-16 11:31:53 +0300

    removed unused elements from start scene

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -10,12 +10,6 @@ using UnityEngine;
 namespace LD48Project {
 	public class StartMenu : GameComponent {
 		[NotNull] public Button StartGame;
-		[NotNull] public Button OpenHowToScreenButton;
-		[Header("how to")] 
-		[NotNull] public Button ReturnToMainMenuButton;
-
-		[NotNull] public GameObject MainScreenRoot;
-		[NotNull] public GameObject HowToScreenRoot;
 		
 		void Start() {
 			// Init external services
@@ -23,20 +17,10 @@ namespace LD48Project {
 			GooglePlayGamesService.Instance.Init();
 			
 			StartGame.onClick.AddListener(() => SceneManager.LoadScene("Gameplay"));			
-			OpenHowToScreenButton.onClick.AddListener(() => {
-				MainScreenRoot.SetActive(false);
-				HowToScreenRoot.SetActive(true);
-			});
-			ReturnToMainMenuButton.onClick.AddListener(() => {
-				MainScreenRoot.SetActive(true);
-				HowToScreenRoot.SetActive(false);
-			});
 		}
 
 		void OnDestroy() {
 			StartGame.onClick.RemoveAllListeners();
-			OpenHowToScreenButton.onClick.RemoveAllListeners();
-			ReturnToMainMenuButton.onClick.RemoveAllListeners();
 		}
 	}
 }
