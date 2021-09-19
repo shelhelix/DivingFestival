6296c3af2b8a8180df2b07c4dc442a39693e25a7
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-09-19 17:14:35 +0300

    removed unnecessary code

--- Assets/Scripts/Utils/GameVersioning/BuildType.cs
+++ Assets/Scripts/Utils/GameVersioning/BuildType.cs
@@ -1,16 +1,6 @@
-﻿using UnityEditor;
-
-namespace LD48Project.Utils.Editor.Build {
+﻿namespace LD48Project.Utils.Editor.Build {
 	public enum BuildType {
 		Internal,
 		Production
-	}
-
-	public static class MenuItems {
-		[MenuItem("Diving Festival/Switch to internal")]
-		static void SwitchToInternal() {
-			foreach ( var buildTargetGroup in new BuildTargetGroup[] { BuildTargetGroup.Android, BuildTargetGroup.WebGL } ) {
-			}
-		}
 	}
 }
