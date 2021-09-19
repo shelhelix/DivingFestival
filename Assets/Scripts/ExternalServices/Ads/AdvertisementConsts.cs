b77a47c4f0642019d543fabba99bdfacceb2c188
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-09-19 16:33:12 +0300

    replaced reward ad with interstitial + hided ad button in webgl release

--- Assets/Scripts/ExternalServices/Ads/AdvertisementConsts.cs
+++ Assets/Scripts/ExternalServices/Ads/AdvertisementConsts.cs
@@ -1,6 +1,6 @@
 ﻿namespace LD48Project.ExternalServices.Ads {
 	public static class AdvertisementConsts {
-		public static string GameId          = "4294653";
-		public static string RewardAdAndroid = "Rewarded_Android";
+		public static string GameId          = "4368839";
+		public static string RewardAdAndroid = "Interstitial_Android";
 	}
 }
---
+++ Assets/Scripts/Utils/NotMobileObjectHider.cs
@@ -0,0 +1,12 @@
+﻿using GameComponentAttributes;
+using LD48Project.Utils.Editor.Build;
+using LD48Project.Utils.GameVersioning;
+using UnityEngine;
+
+namespace LD48Project.Utils {
+	public class NotMobileObjectHider : GameComponent {
+		public void Start() {
+			gameObject.SetActive(Application.isMobilePlatform);
+		}
+	}
+}
